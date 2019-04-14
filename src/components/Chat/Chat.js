9b3ac5769412a199def5145818be59ac535c2911
Leonid Shifrin <leonids@wolfram.com>
2019-04-14 18:37:13 +0300

    Chat component (HW2)

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Message from '../Message';
+import './Chat.css';
+
+class Chat extends Component {
+  changeInputMessage = e => {
+    this.setState({ messageInput: e.target.value });
+  };
+
+  createUUID = (() => {
+    let ctr = 0;
+    return () => {
+      return `msg_${ctr++}`;
+    };
+  })();
+
+  sendMessageOnEnter = e => {
+    console.log(e.key);
+    if (e.key === 'Enter') {
+      const messages = this.state.messages;
+      messages.push({ text: this.state.messageInput });
+      this.setState({
+        messages,
+        messageInput: ''
+      });
+    }
+  };
+
+  state = { messages: [], messageInput: '' };
+
+  render() {
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {this.state.messages.map(msg => (
+              <Message text={msg.text} key={this.createUUID()} />
+            ))}
+          </div>
+        </div>
+        <input
+          className="input-message"
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
