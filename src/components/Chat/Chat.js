39062944e25c16b9abdbcc1ce5dccc186f6e9895
Leonid Shifrin <leonids@wolfram.com>
2019-04-15 20:27:28 +0300

    Improving code style

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,6 +3,8 @@ import Message from '../Message';
 import './Chat.css';
 
 class Chat extends Component {
+  state = { messages: [], messageInput: '' };
+
   changeInputMessage = e => {
     this.setState({ messageInput: e.target.value });
   };
@@ -16,23 +18,21 @@ class Chat extends Component {
 
   sendMessageOnEnter = e => {
     if (e.key === 'Enter') {
-      const messages = this.state.messages;
-      messages.push({ text: this.state.messageInput });
+      const { messages, messageInput } = this.state;
       this.setState({
-        messages,
+        messages: [...messages, { text: messageInput }],
         messageInput: ''
       });
     }
   };
 
-  state = { messages: [], messageInput: '' };
-
   render() {
+    const { messages, messageInput } = this.state;
     return (
       <div className="chat">
         <div className="message-list">
           <div className="messages">
-            {this.state.messages.map(msg => (
+            {messages.map(msg => (
               <Message text={msg.text} key={this.createUUID()} />
             ))}
           </div>
@@ -41,7 +41,7 @@ class Chat extends Component {
           className="input-message"
           onChange={this.changeInputMessage}
           onKeyPress={this.sendMessageOnEnter}
-          value={this.state.messageInput}
+          value={messageInput}
         />
       </div>
     );
